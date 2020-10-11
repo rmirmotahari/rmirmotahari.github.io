a5f1fc0e2f4dcb7eacbd5c53d616916dd4e23707
rmirmotahari <49576793+rmirmotahari@users.noreply.github.com>
2020-10-11 19:04:36 -0400

    Heroku Fixes

--- public/lab_4/server.js
+++ public/lab_4/server.js
@@ -1,8 +1,10 @@
-
 // These are our required libraries to make the server work.
 /* eslint-disable no-console */
 /* eslint-disable no-unused-vars */
 import express from 'express';
+import dotenv from 'dotenv';
+
+dotenv.config();
 
 const app = express();
 const port = process.env.PORT || 3000;
@@ -14,11 +16,11 @@ app.use(express.static('public'));
 app.route('/api')
   .get((req, res) => {
     console.log('GET request detected');
+    res.send(`Lab 5 for ${process.env.NAME}`);
   })
   .post((req, res) => {
     console.log('POST request detected');
-    console.log('Form data submitted', req.body);
-    res.status(200).send('Hello World');
+    console.log('Form data in res.body', req.body);
   });
 
 app.listen(port, () => {
